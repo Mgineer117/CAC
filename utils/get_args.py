d6ab7dc794db879eee794765d51a2fc82027b584
Minjae Cho <whalswo117@gmail.com>
2025-08-03 23:48:52 -0500

    asd

--- utils/get_args.py
+++ utils/get_args.py
@@ -140,7 +140,7 @@ def get_args():
         "--entropy-scaler", type=float, default=1e-2, help="Entropy scaling factor."
     )
     parser.add_argument(
-        "--W-entropy-scaler", type=float, default=1e-0, help="W entropy scaling factor."
+        "--W-entropy-scaler", type=float, default=1e-1, help="W entropy scaling factor."
     )
     parser.add_argument(
         "--control-scaler",
