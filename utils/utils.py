8b561dc89dce306239796ea7e43454064baca9e9
Minjae Cho <whalswo117@gmail.com>
2025-08-06 16:26:06 -0500

    asd

--- utils/utils.py
+++ utils/utils.py
@@ -220,19 +220,19 @@ def get_policy(env, args, Dynamic_func=None):
         #     device=args.device,
         # )
 
-        actor = PPO_Actor(
-            args.state_dim,
-            hidden_dim=args.actor_dim,
-            a_dim=args.action_dim,
-        )
-
-        # actor = C3M_U_Gaussian(
-        #     x_dim=env.num_dim_x,
-        #     state_dim=args.state_dim,
-        #     effective_indices=effective_indices,
-        #     action_dim=args.action_dim,
-        #     task=args.task,
+        # actor = PPO_Actor(
+        #     args.state_dim,
+        #     hidden_dim=args.actor_dim,
+        #     a_dim=args.action_dim,
         # )
+
+        actor = C3M_U_Gaussian(
+            x_dim=env.num_dim_x,
+            state_dim=args.state_dim,
+            effective_indices=effective_indices,
+            action_dim=args.action_dim,
+            task=args.task,
+        )
 
         critic = PPO_Critic(args.state_dim, hidden_dim=args.critic_dim)
 
